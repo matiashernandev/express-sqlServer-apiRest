b717445f7548aeec08e17eacaef5a7ac77a56277
nkwaaaa <nkwaaaa@gmail.com>
2022-07-17 13:50:21 -0300

    update config.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,11 @@
 import sql from "mssql";
+import config from "../config";
 
 const dbSettings = {
-    user: "hernan",
-    password: "Abc123",
-    server: "localhost",
-    database: "webstore",
+    user: config.dbUser,
+    password: config.dbPassword,
+    server: config.dbServer,
+    database: config.dbDatabase,
     options: {
         encrypt: true, // for azure
         trustServerCertificate: true, // change to true for local dev / self-signed certs
